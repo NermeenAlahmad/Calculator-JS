dc6c97b598e2d0b9f53bd1b4a51737241fe728c8
NermeenAlahmad <nermeen.n.alahmad@gmail.com>
2021-10-15 18:59:49 +0300

    Add JavaScript To Calculator

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// This function clear all the values
+var screen = document.getElementById('result');
+function clearScreen() {
+    screen.value=" ";
+   }
+   
+   // This function display values
+   function display(value) {
+    // document.getElementById("result").value += value;
+    screen.value +=value;
+   }
+   // This function evaluates the expression and return result
+   function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+   }
